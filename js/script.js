2e48896bea11797abd10af46612829a633e45f2b
TomasAhumada <tomasahumadacoronel0$>
2022-11-03 22:23:50 -0300

    Primer Subida

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 class Celular {
 
-    constructor(marca, modelo, anio, memoriaRam, memoria, puntuacion) {
-        this.marca = marca;
-        this.modelo = modelo;
-        this.anio = parseInt(anio);
-        this.memoriaRam = memoriaRam;
-        this.memoria = memoria;
-        this.puntuacion = parseInt(puntuacion);
+    constructor(marca, modelo, anioDeFa, memoriaRam, memoria, valoracion) {
+        this.titulo = marca;
+        this.autor = modelo;
+        this.anio = parseInt(anioDeFa);
+        this.genero = memoriaRam;
+        this.valoracion = parseInt(memoria);
+        this.id = valoracion;
     }
 }
-
 
 const celulares = [
     new Celular('Samsung', 'S 22', 2022, '16 GB', '540 GB', 10),
@@ -19,55 +18,58 @@ const celulares = [
     new Celular('Samsung', 'S 22 Flip', 2022, '12 GB', '1 TB', 10),
 ];
 
-console.log(celulares);
-
 let continuar = true;
 
-
 while (continuar) {
-    let ingreso = prompt('Ingresa los datos del Celular: \n1- Marca\n2- modelo\n3- año de fabricacion\n4- memoria ram\n5- memoria\n6- puntaje del 1 al 10\nTodo esto separado por un espacio por cada categoria. \ningresa una X para finalizar');
 
-    if (ingreso.toUpperCase() == 'X') {
+    let ingreso = prompt('Ingresa los datos del celular:\n-marca\n-modelo\n-año de fabricacion\n-memoria Ram\n-memoria\n-puntaje de 1 a 10\nseparados por un espacio. Ingresa X para finalizar');
+
+    if (ingreso.toUpperCase() == 'X'){
         continuar = false;
         break;
     }
-    let datos = ingreso.split(' ');
+
+    const datos = ingreso;
     console.log(datos);
 
-    const celular = new Celular(datos[0], datos[1], datos[2], datos[3], datos[4], datos[5]);
+    const celular = new Celular(celulares[0], celulares[1], datos[2], datos[3], datos[4], datos[5], datos[6]);
 
-    celulares.push(celular);
+    celulares.push(celulares);
     console.log(celulares);
+
 }
 
-let opcion = prompt('Elegi la opcion que mas desees para mostrarte nuestros dispositivos y asi poder comprarte Uno: \n1 - Marca(A a Z) \n2 - Mejor a Peor puntuado \n3 - Fecha de publicacion (mas viejo a as nuevo)');
+let criterio = prompt('Elegí la opcion que deseas para ver que celular se adapta mejor a vos:\n1 - Marca de telefono ordenado(A a Z)\n2 - marca de telefono de Mayor a menor calificacion \n');
 
-function ordenar(opcion, array){
-    let arrayOrden = array.slice(0);
+    function ordenar(criterio, array) {
+        let arrayOrdenado = array.slice(0);
 
-    switch (opcion) {
-        case '1':
-let nombreAscendete = arrayOrden.sort((a,b)=>a.marca.localeCompare(b.marca));
-return nombreAscendete;
-        case '2':
-            return arrayOrden.sort((a,b)=>a.puntuacion.localeCompare(b,puntuacion));
-            break;
-        case '3':
-            return arrayOrden.sort((a,b)=>a.anio.localeCompare(b,anio));
-            break;
-        default:
-            alert('no es un criterio valido');
-            break;
+        switch (criterio) {
+            case '1':
+                let nombreAscendente = arrayOrdenado.sort((a, b) => a.titulo.localeCompare(b.titulo));
+                return nombreAscendente;
+                
+            case '2':
+                return arrayOrdenado.sort((a, b) => a.anio - b.anio);
+                
+            default:
+                alert('No es un criterio válido, te mostraremos el catálogo sin ordenar');
+                return arrayOrdenado;
+                break;
+        }
     }
-}
 
-function ResultadoDeString(array) {
-    let informacion = '';
+    function crearStringResultado(array) {
+        let info = '';
 
-    array.forEach(infoProducto => {
-        informacion!= 'Marca: ' + infoProducto.titulo + '\nmodelo: ' + infoProducto.titulo + '\nAño de fabricacion: ' + infoProducto.titulo + '\nMemoria ram: ' + infoProducto.titulo + '\nMemoria interna: ' + infoProducto.titulo + '\nValoracion: ' + infoProducto.titulo + 'estrellas\n\n';
-    });
-}
+        array.forEach(elemento => {
+            info += 'marca: ' + elemento.marca + '\nmodelo: ' + elemento.modelo + '\nAño de fabricacion: ' + elemento.anioDeFa +'\nmemoria ram: ' + elemento.memoriaRam +' gb'+ '\nmemoria interna de almacenamiento: ' + elemento.memoria +' gb'+'\nValoración: ' + elemento.valoracion + ' puntos.\n\n'
+        });
 
-alert(ResultadoDeString(ordenar(opcion,celulares)));
+        return info;
+
+    }
+
+    alert(crearStringResultado(ordenar(criterio, celulares)));
+
 
