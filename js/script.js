cc393676bf7c165a73f91c6d53e290f6c838ac07
TomasAhumada <tomasahumadacoronel0$>
2022-11-17 21:03:49 -0300

    Arreglos

--- js/script.js
+++ js/script.js
@@ -1,12 +1,6 @@
 
-const btnAgregar = document.getElementById('buttonAgregar'),
-    btncatalogo = document.getElementById('buttonCatalogo')
-
-
-
-
-
-
+const btnAgregar = document.getElementById('botonAgregar'),
+    btncatalogo = document.getElementById('botonCatalogo');
 
 let ingreso;
 
@@ -26,7 +20,7 @@ const celulares = [
     new Celular('Iphone', '14', 2022, '16 GB', '1 TB', 9),
     new Celular('Xiaomi', 'Redmi 8', 2019, '4 GB', '128 GB', 8),
     new Celular('Samsung', 'A 12', 2019, '4 GB', '64 GB', 8),
-    new Celular('Samsung', 'S 22 Flip', 2022, '12 GB', '1 TB', 10),
+    new Celular('Samsung', 'S 22 Flip', 2022, '12 GB', '1 TB', 10)  
 ];
 
 function agregar() {
@@ -42,21 +36,49 @@ function agregar() {
     console.log(celulares);
 }
 function mostrarCatalogo() {
-    let criterio = confirm('Deseas ver el catalogo o no?');
-    switch (criterio) {
-        case true:
-            let msg = '';
-            for (let celular of celulares) {
-                msg += `\nMarca: ${celular.marca}\nModelo: ${celular.modelo}\nCantidad de memoria interna: ${celular.memoria}\n `;
-            }
-            alert(msg);
-            break;
-        case false:
-            alert('ok cualquier consulta no dude en recargar la pagina');
-            break;
+    let criterio = Swal.fire({
+        title: 'Deseas ver el catalogo o no ?',
+        icon: 'info',
+        iconColor: '#0096c7',
+        showCancelButton: true,
+        confirmButtonText: 'Confirmar',
+        cancelButtonText: 'Cancelar',
+        position: 'top-center',
+        backdrop: '#445566aa'
+    }).then((result)=>{
+    if(result.isConfirmed){
+        let msg = '';
+        for (let celular of celulares) {
+            msg += `\nMarca: ${celular.marca}\nModelo: ${celular.modelo}\nCantidad de memoria interna: ${celular.memoria}\n `;
+        }
+        Swal.fire({
+            title: `${msg}`,
+            icon: 'info',
+            iconColor: '#0096c7',
+            showCancelButton: true,
+            confirmButtonText: 'Confirmar',
+            cancelButtonText: 'Cancelar',
+            position: 'top-center',
+            backdrop: '#445566aa'
+        })
     }
+    else{
+        Swal.fire({
+            title: `Error`,
+            icon: 'error',
+            iconColor: '#0096c7',
+            confirmButtonText: 'Confirmar',
+            position: 'top-center',
+            backdrop: '#445566aa'
+    })
+    }
+})
+}
+btnAgregar.onclick = () => {
+    agregar();
 }
 
-btnAgregar.onclick=()=>{
-    agregar()
+btncatalogo.onclick = () => {
+    mostrarCatalogo();
 }
+
